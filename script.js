5b06f1deb29accc92637fe07793624520bd5304e
Glorypixel <ifeomaglory1998@gmail.com>
2024-06-07 01:38:09 +0100

    changes

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const pictures = ["img1", "img2", "img3", "img4", "img5", "img6", "img7"];
 const buttons = document.querySelectorAll(".btn");
-const imgDiv = document.querySelector(".slider-icon");
+const imgElement = document.getElementById("slider-image");
 
 let counter = 0;
 
 buttons.forEach(function (button) {
   button.addEventListener("click", function (e) {
-    if (button.classList.contains("fa-arrow-right")) {
+    if (button.classList.contains("btn-left")) {
       counter--;
       if (counter < 0) {
         counter = pictures.length - 1;
       }
-      imgDiv.style.background = `url("./images/${pictures[counter]}.jpg")`;
+      imgElement.src = `./images/${pictures[counter]}.jpg`;
     }
-    if (button.classList.contains("fa-arrow-left")) {
+    if (button.classList.contains("btn-right")) {
       counter++;
       if (counter > pictures.length - 1) {
         counter = 0;
       }
-      imgDiv.style.background = `url("./images/${pictures[counter]}.jpg")`;
-      //   imgDiv.style.width = "80%";
+      imgElement.src = `./images/${pictures[counter]}.jpg`;
     }
   });
 });
